5983da7e7ea822258cc07dde4dba341576bb0207
eshankuthu <kuthu.eshan@gmail.com>
2017-08-01 22:41:00 -0500

    Basic Spring AOP

--- exercise6_1/src/main/java/cs544/exercise6_1/aspect/AspectLogger.java
+++ exercise6_1/src/main/java/cs544/exercise6_1/aspect/AspectLogger.java
@@ -2,17 +2,41 @@ package cs544.exercise6_1.aspect;
 
 import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.util.StopWatch;
+import cs544.exercise6_1.EmailSender;
 
 @Aspect
 public class AspectLogger {
 	
 	final static Logger logger = Logger.getLogger(AspectLogger.class);
 	
-	@After("execution(public void sendEmail(..))")
-	public void logger(JoinPoint joinpoint){
-		logger.info("method: "+joinpoint.getSignature().getName());	
+	@After("execution(public void sendEmail(String,String)) && args(email,address)")
+	public void logger(JoinPoint joinpoint, String email, String address){
+//		logger.info("method= "+joinpoint.getSignature().getName());	
+		
+		EmailSender sendEmail  = (EmailSender)joinpoint.getTarget();
+		
+		System.out.println("method = " + joinpoint.getSignature().getName());
+		System.out.println("address = " + email);
+		System.out.println("message = " + address);
+		System.out.println("outgoing mail server = " + sendEmail.getOutgoingMailServer());
+	}
+	
+	@Around("execution(public void save(*))")
+	public Object invoke(ProceedingJoinPoint call ) throws Throwable {
+		StopWatch sw = new StopWatch();
+		sw.start(call.getSignature().getName());
+		Object retVal = call.proceed();
+		sw.stop();
+		long totaltime = sw.getLastTaskTimeMillis();
+		// print the time to the console
+		System.out.println("Time to execute save = " + totaltime + " ms");
+		return retVal;
 	}
 
 }
